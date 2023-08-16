407dcb2e7b881ac642c09196bdfd7d6386c52cc7
DefinetlyNotMe <simon.foerster@uni-weimar.de>
2023-08-16 21:44:03 +0200

    debugginh

--- venv/ReformCorpus.py
+++ venv/ReformCorpus.py
@@ -14,6 +14,8 @@ def analyse_and_seperate(path):
     # what if texts are only used as not same author
     #only possible if we compare n^2 pairs
     # first compare all to get the complete picture of authors -> look wether all authors are complete. de
+    same_author_pairs  = 0
+    different_author_pairs = 0
     print(f"There are {corpus.review.shape} pairs")
     for idx in tqdm(range(corpus.review.shape[0]), desc='Analysing Corpus'):
 
@@ -23,6 +25,7 @@ def analyse_and_seperate(path):
         total_texts.update([doc_1,doc_2])
 
         if corpus.sentiment[idx] == 1:
+            same_author_pairs += 1
             # if either text is already in corpus match them
             found = False
             for author in authors:
@@ -34,6 +37,7 @@ def analyse_and_seperate(path):
             if not found:
                 authors.append({doc_1,doc_2})
         else:
+            different_author_pairs += 1
             not_same_author.append([doc_1,doc_2])
 
     count = 0
@@ -43,11 +47,12 @@ def analyse_and_seperate(path):
         texts_from_authors.union(author)
         total_texts_under_authors += len(author)
         if(len(texts_from_authors) != total_texts_under_authors):
-            print('Wrong amaount of texts in: ' +str(count))
+            print(f'Wrong amaount of texts in: {str(count)}. There are {texts_from_authors} text in the set, but we count {total_texts_under_authors}.')
         count += 1
 
     print(f'There are {len(authors)} authors with an average of {total_texts_under_authors/len(authors)}({0})sd texts per author.')
     print(f'There are {total_texts_under_authors} texts from {len(total_texts)} total texts')
+    print(f'There are {same_author_pairs} Same author Pairs and {different_author_pairs} different Author pairs.')
     print(f'Test: Length of all Text from the authors: {len(set().union(*authors))}')
 if __name__ == '__main__':
     args = sys.argv[1:]
