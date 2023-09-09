f92c196c3eef9fd1de600fd0c701955476a1b052
DefinetlyNotMe <simon.foerster@uni-weimar.de>
2023-09-09 13:34:39 +0200

    remove small and big texts, seperate texts into different files based on author

--- venv/ReformCorpus.py
+++ venv/ReformCorpus.py
@@ -7,58 +7,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import json
 from collections import defaultdict
-
-def analyse_and_seperate(path):
-    corpus = pd.read_csv(path, sep='\t')
-    #each author is represented as the sum of their texts
-    authors = []
-    not_same_author = []
-    total_texts = set()
-    #is the sum of their text all text ?
-    # what if texts are only used as not same author
-    #only possible if we compare n^2 pairs
-    # first compare all to get the complete picture of authors -> look wether all authors are complete. de
-    same_author_pairs  = 0
-    different_author_pairs = 0
-    print(f"There are {corpus.review.shape} pairs")
-    for idx in tqdm(range(corpus.review.shape[0]), desc='Analysing Corpus'):
-
-        temp = corpus.review[idx].split('$$$')
-        doc_2 = BeautifulSoup(temp[0], 'html.parser').get_text().encode('utf-8').decode('utf-8')
-        doc_1 = BeautifulSoup(temp[1], 'html.parser').get_text().encode('utf-8').decode('utf-8')
-        total_texts.update([doc_1,doc_2])
-
-        if corpus.sentiment[idx] == 1:
-            same_author_pairs += 1
-            # if either text is already in corpus match them
-            found = False
-            for author in authors:
-                for text in author:
-                    if doc_1 == text or doc_2 == text:
-                        found = True
-                        author.update([doc_1,doc_2])
-                        break
-            if not found:
-                authors.append({doc_1,doc_2})
-        else:
-            different_author_pairs += 1
-            not_same_author.append([doc_1,doc_2])
-
-    count = 0
-    total_texts_under_authors = 0
-    texts_from_authors = set()
-    for author in authors:
-        texts_from_authors = texts_from_authors.union(author)
-        total_texts_under_authors += len(author)
-        if(len(texts_from_authors) != total_texts_under_authors):
-            print(f'Wrong amount of texts in: {str(count)}. There are {len(texts_from_authors)} text in the set, but we count {total_texts_under_authors}.')
-        count += 1
-
-    print(f'There are {len(authors)} authors with an average of {round(total_texts_under_authors/len(authors),3)} texts per author.')
-    print(f'There are {total_texts_under_authors} texts from {len(total_texts)} total texts')
-    print(f'There are {same_author_pairs} Same author Pairs and {different_author_pairs} different Author pairs.')
-    print(f'Test: Length of all Text from the authors: {len(set().union(*authors))}')
-def analyse(jsonl_file):
+def analyse(jsonl_file, min_token = 500, max_token = 1000):
+    base = "data"
     nlp = spacy.load("en_core_web_lg")
 
     with open(jsonl_file, 'r') as file1:
@@ -95,17 +45,44 @@ def analyse(jsonl_file):
             author_L, author_R = data_L['id'], data_R['id']
             topic_L, topic_R = data_L['topic'], data_R['topic']
 
+            #create jsonl for new filesystem
+            data_to_append = {"topic": topic_L, "review":data_L['review'] }
+
+            #make file with author id if necessary, else append a jsonl with the text.
+            if token_count_L >= 500 and token_count_L <= max_token:
+                file_path = os.path.join(base,author_L)
+                if os.path.exists(file_path):
+                    with open(file_path, 'a') as file:
+                        file.write(data_to_append + '\n')
+                else:
+                    with open(file_path, 'w') as file:
+                        file.write(data_to_append + '\n')
+
+            data_to_append = {"topic": topic_R, "review":data_R['review'] }
+
+            if token_count_R > min_token and token_count_R <= max_token:
+                file_path = os.path.join(base,author_R)
+                if os.path.exists(file_path):
+                    with open(file_path, 'a') as file:
+                        file.write(data_to_append + '\n')
+                else:
+                    with open(file_path, 'w') as file:
+                        file.write(data_to_append + '\n')
+
+
             def increment_value(dictionary, key):
                 current_value = dictionary.get(key, 0)
                 dictionary[key] = current_value + 1
                 return dictionary
 
             #count author texts and topics
-            authors[author_L] = (authors[author_L][0] + 1,increment_value(authors[author_L][1],topic_L))
-            authors[author_R]  = (authors[author_R][0] + 1,increment_value(authors[author_R][1],topic_R))
+            if token_count_L > min_token and token_count_L <= max_token:
+                authors[author_L] = (authors[author_L][0] + 1,increment_value(authors[author_L][1],topic_L))
+                lengths.append(token_count_L)
 
-            lengths.append(token_count_L)
-            lengths.append(token_count_R)
+            if token_count_R > min_token and token_count_R <= max_token:
+                authors[author_R]  = (authors[author_R][0] + 1,increment_value(authors[author_R][1],topic_R))
+                lengths.append(token_count_R)
 
         #analyse data
         #Text length
@@ -134,6 +111,15 @@ def analyse(jsonl_file):
         print(f"Standard Deviation of Texts per Author: {std_dev_texts_per_author}")
         print(f"Average Topics per Author: {avg_topics_per_author}")
         print(f"Standard Deviation of Topics per Author: {std_dev_topics_per_author}")
+
+        with open('results_dict', 'w') as file:
+            file.write(authors)
+
+        return authors
+def make_pairs():
+    #remove text if to short,
+    #seperate a test set with seperate authors
+    #mix and match pairs
 if __name__ == '__main__':
     args = sys.argv[1:]
     analyse(args[0])
