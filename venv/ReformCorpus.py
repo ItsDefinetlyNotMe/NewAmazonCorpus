2ee7d5d3f319e6f61057765507d63c10df1e5a28
DefinetlyNotMe <simon.foerster@uni-weimar.de>
2023-09-16 12:04:58 +0200

    making and saving test and training set as csv

--- venv/ReformCorpus.py
+++ venv/ReformCorpus.py
@@ -1,15 +1,19 @@
 import os
+import random
 import sys
 import spacy
 import statistics
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 import pandas as pd
+import csv
 import json
 from collections import defaultdict
 def analyse(jsonl_file, min_token = 500, max_token = 1000):
     base = "data"
     nlp = spacy.load("en_core_web_lg")
+
+#read test file aswell
 
     with open(jsonl_file, 'r') as file1:
         lengths = []
@@ -116,11 +120,186 @@ def analyse(jsonl_file, min_token = 500, max_token = 1000):
             file.write(json.dumps(authors))
 
         return authors
-def make_pairs():
-    pass
-    #remove text if to short,
-    #seperate a test set with seperate authors
-    #mix and match pairs
+def make_pairs(my_dict):
+    my_array = my_dict.keys()
+
+    #allow text to be x times in the set
+    appearences = 2
+    #how much % is test_set exclusive
+    p_test = 0.1
+    test_authors ={}
+    training_authors = {}
+
+    # Calculate the number of elements to select
+    num_to_select = int(p_test * len(my_array))
+
+    # seperate a test set with seperate authors
+    selected_elements = random.sample(my_array, num_to_select)
+    test_authors = dict.fromkeys(selected_elements)
+    training_authors = dict.fromkeys([x for x in my_array if x not in selected_elements])
+
+
+    test_set_number_texts = 0
+    for author in test_authors.keys():
+        text_numbers = my_dict[author][0]
+        array = []
+        for text_number in text_numbers:
+            test_set_number_texts += text_number
+            array.append([text_number]*appearences)
+
+        test_authors[author] = array
+
+    training_set_number_texts = 0
+    for author in training_authors.keys():
+        text_numbers = my_dict[author][0]
+        array = []
+        for text_number in text_numbers:
+            training_set_number_texts += text_number
+            array.append([text_number]*appearences)
+        training_authors[author] = array
+
+    #make pairs
+    #50/50
+    training_set_diff_number = int(training_set_number_texts * 0.5)
+    training_set_same_number = training_set_number_texts  - training_set_diff_number
+    training_set = []
+
+    for _ in range(training_set_same_number):
+        if any(len(set(training_authors[element])) > 2 for element in training_authors.keys()):
+            while True:
+                first_text = 0
+                second_text = 0
+                random_author = random.choice(training_authors.keys())
+                text_set = set(training_authors[random_author])
+                if len(text_set) > 1:
+                    first_text = random.choice(text_set)
+                    text_set.remove(first_text)
+                    second_text = random.choice(text_set)
+                    training_authors[random_author].remove(first_text)
+                    training_authors[random_author].remove(second_text)
+                else:
+                    continue
+                training_set.append((random_author,first_text),(random_author,second_text))
+                break
+        print("Out of same author texts")
+
+    for _ in range(training_set_diff_number):
+        if len( training_authors.keys()) > 1:
+            random_author_1 = random.choice(training_authors.keys())
+            random_author_2 = random_author_1
+            while random_author_2 == random_author_1:
+                random_author_2 = random.choice(training_authors.keys())
+            text_1 = random.choice(set(training_authors[random_author_1]))
+            training_authors[random_author_1].remove(text_1)
+            if len(training_authors[random_author_1]) < 1:
+                del training_authors[random_author_1]
+            text_2 = random.choice(set(training_authors[random_author_2]))
+            training_authors[random_author_2].remove(text_2)
+            if len(training_authors[random_author_2]) < 1:
+                del training_authors[random_author_2]
+            training_set.append((random_author_1, text_1),(random_author_2, text_2))
+
+    #get same number  
+
+    # 90/10
+    test_set_diff_number = int(test_set_number_texts * 0.9)
+    test_set_same_number = test_set_number_texts  - test_set_diff_number
+    test_set = []
+
+    for _ in range(test_set_same_number):
+        if any(len(set(test_authors[element])) > 2 for element in training_authors.keys()):
+            while True:
+                first_text = 0
+                second_text = 0
+                random_author = random.choice(test_authors.keys())
+                text_set = set(test_authors[random_author])
+                if len(text_set) > 1:
+                    first_text = random.choice(text_set)
+                    text_set.remove(first_text)
+                    second_text = random.choice(text_set)
+                    test_authors[random_author].remove(first_text)
+                    test_authors[random_author].remove(second_text)
+                else:
+                    continue
+                test_set.append((random_author, first_text), (random_author, second_text))
+                break
+        print("Out of same author texts")
+
+    for _ in range(test_set_diff_number):
+        if len(test_authors.keys()) > 1:
+            random_author_1 = random.choice(test_authors.keys())
+            random_author_2 = random_author_1
+            while random_author_2 == random_author_1:
+                random_author_2 = random.choice(test_authors.keys())
+            text_1 = random.choice(set(test_authors[random_author_1]))
+            test_authors[random_author_1].remove(text_1)
+            if len(test_authors[random_author_1]) < 1:
+                del test_authors[random_author_1]
+            text_2 = random.choice(set(test_authors[random_author_2]))
+            test_authors[random_author_2].remove(text_2)
+            if len(test_authors[random_author_2]) < 1:
+                del test_authors[random_author_2]
+            test_set.append((random_author_1, text_1), (random_author_2, text_2))
+
+    #shuffle sets
+    random.shuffle(training_set)
+    random.shuffle(test_set)
+    print("finished sets")
+    return training_set,test_set
+
+def write_csv_from_sets(training_set,test_set):
+    field_names = ["id", "sentiment", "review"]
+    with open("training.csv",mode="w+", newline="") as csvfile:
+        writer = csv.DictWriter(file, fieldnames=field_names)
+        writer.writeheader()
+        counter = 0
+        for pair in tqdm(training_set, desc="Writing_Training_set"):
+            author_1 =  pair[0][0]
+            author_2 = pair[1][0]
+            same_author = author_1 == author_2
+
+            text_1 = ""
+            with open(author_1, 'r') as file:
+                text_1 = json.loads(next(islice(file, pair[0][1] - 1, pair[0][1]), None)).get('review', '')
+
+            text_2 = ""
+            with open(author_2, 'r') as file:
+                text_2 = json.loads(next(islice(file, pair[1][1] - 1, pair[1][1]), None)).get('review', '')
+
+            text = text_1 + "$$$" + text_2
+            row = {'id':counter, 'sentiment':same_author,'review':text}
+            writer.writerow(row)
+
+    with open("test.csv",mode="w+", newline="") as csvfile:
+        writer = csv.DictWriter(file, fieldnames=field_names)
+        writer.writeheader()
+        counter = 0
+        for pair in tqdm(test_set, desc="Writing_Training_set"):
+            author_1 =  pair[0][0]
+            author_2 = pair[1][0]
+            same_author = author_1 == author_2
+
+            text_1 = ""
+            topic_1 = ""
+            with open(author_1, 'r') as file:
+                json_1 = json.loads(next(islice(file, pair[0][1] - 1, pair[0][1]), None))
+                text_1 = json_1.get('review', '')
+                topic_1 = json_1.get('topic', '')
+
+            text_2 = ""
+            topic_2 = ""
+            with open(author_2, 'r') as file:
+                json_2 = json.loads(next(islice(file, pair[1][1] - 1, pair[1][1]), None))
+                text_2.get('review', '')
+                topic_2 = json_2.get('topic', '')
+
+            text = text_1 + "$$$" + text_2
+            row = {'id':counter, 'sentiment':same_author,'topic':topic_1 == topic_2, 'review':text}
+            writer.writerow(row)
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
-    analyse(args[0])
+    author_dict = analyse(args[0])
+    training_set,test_set = make_pairs(author_dict)
+    write_csv_from_sets(training_set, test_set)
